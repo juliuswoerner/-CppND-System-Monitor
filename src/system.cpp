62880d94bcf4e7654062de0510816e5614827745
juliuswoerner <j14wq73@gmail.com>
2022-08-03 00:05:59 +0200

    system

--- src/system.cpp
+++ src/system.cpp
@@ -36,4 +36,4 @@ int System::RunningProcesses() { return running_processes_; }
 int System::TotalProcesses() { return total_processes_; }
 
 // TODO: Return the number of seconds since the system started running
-long int System::UpTime() { return LinuxParser::UpTime; }
+long System::UpTime() { return LinuxParser::UpTime() ; }
