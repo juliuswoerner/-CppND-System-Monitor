b57226a16e62507f4ddeb2088b78afa3bf1280de
juliuswoerner <j14wq73@gmail.com>
2022-08-03 04:09:22 +0200

    asf

--- src/process.cpp
+++ src/process.cpp
@@ -24,10 +24,10 @@ float Process::CpuUtilization() {
 string Process::Command() { return LinuxParser::Command(pid_); }
 
 // TODO: Return this process's memory utilization
-string Process::Ram() { return LinuxParser::Ram(pid_) }
+string Process::Ram() { return LinuxParser::Ram(pid_); }
 
 // TODO: Return the user (name) that generated this process
-string Process::User() { return LinuxParser::User(pid_) }
+string Process::User() { return LinuxParser::User(pid_); }
 
 // TODO: Return the age of this process (in seconds)
 long int Process::UpTime() { return LinuxParser::UpTime(pid_); }
