e52410ea29af924eef8e157eb800cdf4ddb0ba3c
juliuswoerner <j14wq73@gmail.com>
2022-08-03 02:02:07 +0200

    ad

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,16 @@
 #include "processor.h"
+#include "linux_parser.h"
+#include <string>
+#include <vector>
+
+using std::stof;
+using std::string;
+using std::to_string;
+using std::vector;
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return utilization_; }
+float Processor::Utilization() { 
+    vector<string> utilizations = LinuxParser::CpuUtilization();
+
+    return stof(utilizations[4]); 
+}
