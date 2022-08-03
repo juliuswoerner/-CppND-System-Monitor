ea88bc78db2d695313bf198622e4ced602eec109
juliuswoerner <j14wq73@gmail.com>
2022-08-03 03:03:45 +0200

    asf

--- src/processor.cpp
+++ src/processor.cpp
@@ -12,6 +12,6 @@ using std::vector;
 float Processor::Utilization() { 
     vector<string> utilizations = LinuxParser::CpuUtilization();
     string test = utilizations.at(4);
-    float test_float = std::stoi(utilizations[4]);
+    float test_float = std::stof(utilizations[4]);
     return test_float; 
 }
