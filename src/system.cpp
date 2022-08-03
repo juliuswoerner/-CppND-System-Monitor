e29d71a383f25b691d96f149e745615a080ba28a
juliuswoerner <j14wq73@gmail.com>
2022-08-03 09:17:03 +0200

    as

--- src/system.cpp
+++ src/system.cpp
@@ -18,7 +18,14 @@ using std::vector;
 Processor& System::Cpu() { return cpu_; }
 
 // TODO: Return a container composed of the system's processes
-vector<Process>& System::Processes() { return processes_; }
+vector<Process>& System::Processes() { 
+    vector<Process> processes;
+    vector<int> pids = LinuxParser::Pids();
+    for(int pid : pids){
+        Process process(pid);
+        processes.push_back(process);
+    }
+    return processes; }
 
 // TODO: Return the system's kernel identifier (string)
 std::string System::Kernel() { return LinuxParser::Kernel(); }
