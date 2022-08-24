e94c2d27a3b4e98a1e2be9ece8950a4f6f154513
juliuswoerner <j14wq73@gmail.com>
2022-08-24 14:18:28 +0200

    asfg

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -166,13 +166,24 @@ string LinuxParser::Command(int pid) {
 // TODO: Read and return the memory used by a process
 // REMOVE: [[maybe_unused]] once you define the function
 string LinuxParser::Ram(int pid) { 
-  string attribute, v[23], line;
+  string attribute, v[23], line, key, memStr;
+  int mem;
   std::ifstream stream(kProcDirectory + "/" + std::to_string(pid) + kStatFilename);
-  while (stream.is_open()) {
-    std::getline(stream, line);
+  // while (stream.is_open()) {
+  //   std::getline(stream, line);
+  //   std::istringstream linestream(line);
+  //   linestream >> v[0] >> v[1] >> v[2] >> v[3] >> v[4] >> v[5] >> v[6] >> v[7] >> v[8] >> v[9] >> v[10] >> v[11] >> v[12] >> v[13] >> v[14] >> v[15] >> v[16] >> v[17] >> v[18] >> v[19] >> v[20] >> v[21] >> v[22] ;
+  //   return v[22];
+  // }
+  while (std::getline(stream, line)) {
     std::istringstream linestream(line);
-    linestream >> v[0] >> v[1] >> v[2] >> v[3] >> v[4] >> v[5] >> v[6] >> v[7] >> v[8] >> v[9] >> v[10] >> v[11] >> v[12] >> v[13] >> v[14] >> v[15] >> v[16] >> v[17] >> v[18] >> v[19] >> v[20] >> v[21] >> v[22] ;
-    return v[22];
+    linestream >> key;
+    if (key == "VmSize:"){
+      linestream >> memStr;
+      mem = stoi(memStr)/1024;
+      memStr = std::to_string(mem);
+      return 
+    }
   }
   return ""; 
   }
