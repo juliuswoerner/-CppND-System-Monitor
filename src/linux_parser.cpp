e5e80673990b019408aae4bed49544958411d411
juliuswoerner <j14wq73@gmail.com>
2022-08-25 22:16:54 +0200

    asdg

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -176,7 +176,7 @@ string LinuxParser::Ram(int pid) {
     linestream >> v[0] >> v[1] >> v[2] >> v[3] >> v[4] >> v[5] >> v[6] >> v[7] >> v[8] >> v[9] >> v[10] >> v[11] >> v[12] >> v[13] >> v[14] >> v[15] >> v[16] >> v[17] >> v[18] >> v[19] >> v[20] >> v[21] >> v[22] ;
     memStr = v[22];
     std::cout << "check 5" << std::endl;
-    if(memStr == "") {memStr = "0";}
+    std::cout << "memStr: " << memStr << std::endl;
     mem = stoi(memStr)/1024;
     std::cout << "check 6" << std::endl;
     memStr = std::to_string(mem);
