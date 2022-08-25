9ff4364416942332cddc9a8ce6cf70ab06efa589
juliuswoerner <j14wq73@gmail.com>
2022-08-25 21:44:37 +0200

    asdf

--- src/system.cpp
+++ src/system.cpp
@@ -23,10 +23,11 @@ vector<Process>& System::Processes() {
     processes_.clear();
     vector<int> pids = LinuxParser::Pids();
     for(auto pid : pids){
-        // if(LinuxParser::Ram(pid) != "0"){
-        //     processes_.emplace_back(pid);
-        // }
-        processes_.emplace_back(pid);
+        if(LinuxParser::Ram(pid) != "0"){
+            std::cout << "pid:" << pid << std::endl; 
+            processes_.emplace_back(pid);
+        }
+        //processes_.emplace_back(pid);
     }
     std::sort(processes_.begin(), processes_.end());
     return processes_;
