d0558220458f054528b3583b23c58ce598c1e0d7
juliuswoerner <j14wq73@gmail.com>
2022-08-26 16:56:03 +0200

    asdg

--- src/processor.cpp
+++ src/processor.cpp
@@ -13,3 +13,18 @@ float Processor::Utilization() {
     vector<string> utilizations = LinuxParser::CpuUtilization();
     return std::stof(utilizations[0]) / (std::stof(utilizations[0]) + std::stof(utilizations[3])); 
 }
+
+float Processor:Hertz() {
+    float Hertz;
+    std::ifstream stream("/proc" + "/" + "cpuinfo");
+    while (stream.is_open()) {
+        std::getline(stream, line);
+        std::istringstream linestream(line);
+        linestream >> attribute >> value;
+        if(attribute == "cpu MHz"){
+         Hertz = stof(value);
+         return 1000000 * Hertz;
+        }
+    }
+
+}
